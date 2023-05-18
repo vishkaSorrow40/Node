b54f491411cab9fd03524d572948c94fa1103310
viskaSorrow40 <vika_akiv_v@mail.ru>
2023-05-19 02:39:14 +0300

    meme user

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ const port= 5500
 
 
 const server = http.createServer((req, res)=>{
-    res.setHeader("Content-Type", "text/plain");
-    res.end("yra! this is my server, no gde je on bil a week ago on kontrolnyi work :((((((");
+    if(req.url ==="/"){
+        res.statusCode=200;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("yra! this is my server, no gde je on bil a week ago on kontrolnyi work :((((((");
+    }else if(req.url === "/user"){
+        res.statusCode=200;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("vozradyisia USER  this is my server, no gde je on bil a week ago on kontrolnyi work :((((((");
+    } else{
+        res.statusCode=404;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("USER  ty slomal moi server, mne teper grystno i nevkusno:((((((");
+    }
+
+    
+    
 });
 
 server.on("connection", ()=>{
