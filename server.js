1cae18f620b5e547fa5c95e8b3c47f118f5ed15f
viskaSorrow40 <vika_akiv_v@mail.ru>
2023-05-19 05:11:46 +0300

    almost done

--- server.js
+++ server.js
@@ -1,31 +1,60 @@
 const { log } = require('node:console');
 const http=require('node:http');
+const { connect } = require('node:http2');
 
 const host= "127.0.0.1"
 const port= 5500
 
+var users={'user_agent': 0};
+var comm=[];
 
 const server = http.createServer((req, res)=>{
     if(req.url ==="/"){
-        res.statusCode=200;
-        res.setHeader("Content-Type", "text/plain");
-        res.end("yra! this is my server, no gde je on bil a week ago on kontrolnyi work :((((((");
-    }else if(req.url === "/user"){
-        res.statusCode=200;
-        res.setHeader("Content-Type", "text/plain");
-        res.end("vozradyisia USER  this is my server, no gde je on bil a week ago on kontrolnyi work :((((((");
-    } else{
+        if(req.method === "GET"){
+            res.statusCode=200;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("yra! this is my server, no gde je on bil a week ago on kontrolnyi work :((((((");
+        }
+    }else if(req.url === "/stats"){
+        if(req.method=== "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            users.user_agent++;
+            res.end(`<table>
+            <tr>
+                <th>User-agent:</th>
+            </tr>
+            <tr>
+                <td>${users.user_agent}</td>
+            </tr>
+            </table>`);
+        }
+    } else if(req.url === "/comments"){
+        if(req.method=== "POST"){
+            res.statusCode=200;
+            res.setHeader("Content-Type", "application/json");
+            body="";
+            req.on("data", (chunk)=>{
+                body+=chunk;
+            });
+            req.on("end", ()=>{
+                comm.push(JSON.parse(body));
+                res.end(JSON.stringify(comm));
+
+            });
+   
+        }
+    }
+    else{
         res.statusCode=404;
         res.setHeader("Content-Type", "text/plain");
         res.end("USER  ty slomal moi server, mne teper grystno i nevkusno:((((((");
     }
 
-    
-    
 });
 
 server.on("connection", ()=>{
     console.log("New connection!");
+    console.log(`ip ${host}, port: ${port}`);
 });
 
 server.listen(port, host, ()=> {
